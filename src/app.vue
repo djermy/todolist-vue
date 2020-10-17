1da6743cc310ca42f9db2d3108fcbfc9e25d1396
shadowsoultaker <super_red_raider@aol.com>
2020-10-17 02:17:13 +0200

    initial commit

---
+++ src/app.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <img alt="Notepad Icon" src="./assets/notepad.png" />
+    <div class="container">
+      <todo-card v-for="(todoItem, i) in todoItems" :key="i" :todoObject="todoItem"></todo-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoCard from "./components/todo-card.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoCard,
+  },
+  data: function () {
+    return {
+      someText: "hello my name is dan",
+      todoItems: [
+        {todo: 'Do the washing up'},
+        {todo: 'take out the bins'},
+        {todo: 'clean bathroom'},
+        {todo: 'feed cats'},
+      ] 
+    };
+  },
+  methods: {},
+  mounted: function () {
+    setTimeout(() => {
+      this.someText = "no my name is james";
+    }, 5000);
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/todo-card.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="todo-card">
+        {{ todoObject.todo }}
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'todo-card',
+    props: ['todoObject']
+}
+</script>
+
+<style scoped>
+.todo-card {
+    border-radius: 5px;
+    box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.4);
+    padding: 5px 10px;
+    margin: 20px 0;
+}
+</style>
